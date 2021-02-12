b87e6843ca8dfaf4410b003664549c78d7b7bd13
Hürol Aslan <freebelion@gmail.com>
2021-02-12 16:23:53 +0300

    Ders2 Projesi Oluşturuldu

---
+++ Ders2_HesapMakinesi/AnaForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Ders2_HesapMakinesi
+{
+   partial class AnaForm
+   {
+      /// <summary>
+      ///Gerekli tasarımcı değişkeni.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///Kullanılan tüm kaynakları temizleyin.
+      /// </summary>
+      ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer üretilen kod
+
+      /// <summary>
+      /// Tasarımcı desteği için gerekli metot - bu metodun 
+      ///içeriğini kod düzenleyici ile değiştirmeyin.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.SuspendLayout();
+         // 
+         // AnaForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(442, 320);
+         this.Name = "AnaForm";
+         this.Text = "Form1";
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+   }
+}
+
---
+++ Ders2_HesapMakinesi/AnaForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders2_HesapMakinesi
+{
+   public partial class AnaForm : Form
+   {
+      private const int bosluk = 5;
+      private TextBox tbEkran;
+      public AnaForm()
+      {
+         InitializeComponent();
+
+         this.Load += AnaForm_Load;
+         this.SizeChanged += AnaForm_BoyutDegisti;
+      }
+
+      private void AnaForm_Load(object sender, EventArgs e)
+      {
+         this.tbEkran = new TextBox();
+         this.Controls.Add(tbEkran);
+         this.MinimumSize = new Size(200, 250);
+
+         FormuDuzenle();
+      }
+
+      private void AnaForm_BoyutDegisti(object sender, EventArgs e)
+      {
+         FormuDuzenle();
+      }
+
+      private void FormuDuzenle()
+      {
+         this.tbEkran.Left = bosluk;
+         this.tbEkran.Top = bosluk;
+         tbEkran.Width = this.ClientRectangle.Width - 2 * bosluk;
+      }
+   }
+}
