2d998a00efb03bf9858a22142e1dd2f811131380
Hürol Aslan <freebelion@gmail.com>
2021-03-08 15:42:20 +0300

    Ders9 projesi eklendi

---
+++ Ders9_OyunProje1/OyunForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Ders9_OyunProje1
+{
+   partial class OyunForm
+   {
+      /// <summary>
+      ///Gerekli tasarımcı değişkeni.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///Kullanılan tüm kaynakları temizleyin.
+      /// </summary>
+      ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer üretilen kod
+
+      /// <summary>
+      /// Tasarımcı desteği için gerekli metot - bu metodun 
+      ///içeriğini kod düzenleyici ile değiştirmeyin.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.components = new System.ComponentModel.Container();
+         this.KontrolSeriti = new System.Windows.Forms.ToolStrip();
+         this.btnBaslat = new System.Windows.Forms.ToolStripButton();
+         this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+         this.tbZamanAralik = new System.Windows.Forms.ToolStripTextBox();
+         this.Zamanlayici = new System.Windows.Forms.Timer(this.components);
+         this.OyunPanel = new System.Windows.Forms.Panel();
+         this.KontrolSeriti.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // KontrolSeriti
+         // 
+         this.KontrolSeriti.BackColor = System.Drawing.Color.Peru;
+         this.KontrolSeriti.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnBaslat,
+            this.toolStripLabel1,
+            this.tbZamanAralik});
+         this.KontrolSeriti.Location = new System.Drawing.Point(0, 0);
+         this.KontrolSeriti.Name = "KontrolSeriti";
+         this.KontrolSeriti.Size = new System.Drawing.Size(800, 25);
+         this.KontrolSeriti.TabIndex = 0;
+         this.KontrolSeriti.Text = "toolStrip1";
+         // 
+         // btnBaslat
+         // 
+         this.btnBaslat.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+         this.btnBaslat.Image = global::Ders9_OyunProje1.Properties.Resources.imgBaslat;
+         this.btnBaslat.ImageTransparentColor = System.Drawing.Color.Magenta;
+         this.btnBaslat.Name = "btnBaslat";
+         this.btnBaslat.Size = new System.Drawing.Size(23, 22);
+         this.btnBaslat.Text = "Başlat";
+         this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+         // 
+         // toolStripLabel1
+         // 
+         this.toolStripLabel1.Name = "toolStripLabel1";
+         this.toolStripLabel1.Size = new System.Drawing.Size(84, 22);
+         this.toolStripLabel1.Text = "Zaman Aralığı ";
+         // 
+         // tbZamanAralik
+         // 
+         this.tbZamanAralik.Font = new System.Drawing.Font("Segoe UI", 9F);
+         this.tbZamanAralik.Name = "tbZamanAralik";
+         this.tbZamanAralik.Size = new System.Drawing.Size(100, 25);
+         this.tbZamanAralik.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbZamanAralik_KeyDown);
+         // 
+         // Zamanlayici
+         // 
+         this.Zamanlayici.Interval = 500;
+         this.Zamanlayici.Tick += new System.EventHandler(this.Zamanlayici_Tick);
+         // 
+         // OyunPanel
+         // 
+         this.OyunPanel.BackColor = System.Drawing.Color.MediumBlue;
+         this.OyunPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.OyunPanel.Location = new System.Drawing.Point(0, 25);
+         this.OyunPanel.Name = "OyunPanel";
+         this.OyunPanel.Size = new System.Drawing.Size(800, 425);
+         this.OyunPanel.TabIndex = 1;
+         // 
+         // OyunForm
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(800, 450);
+         this.Controls.Add(this.OyunPanel);
+         this.Controls.Add(this.KontrolSeriti);
+         this.Name = "OyunForm";
+         this.Text = "Hedef Vurma Oyunu";
+         this.Load += new System.EventHandler(this.OyunForm_Load);
+         this.KontrolSeriti.ResumeLayout(false);
+         this.KontrolSeriti.PerformLayout();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.ToolStrip KontrolSeriti;
+      private System.Windows.Forms.ToolStripButton btnBaslat;
+      private System.Windows.Forms.Timer Zamanlayici;
+      private System.Windows.Forms.Panel OyunPanel;
+      private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+      private System.Windows.Forms.ToolStripTextBox tbZamanAralik;
+   }
+}
+
---
+++ Ders9_OyunProje1/OyunForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders9_OyunProje1
+{
+   public partial class OyunForm : Form
+   {
+      private Graphics panelGrafik;
+      private Properties.Settings ayarlar;
+
+      private int hedefBoyutu = 10;
+      private Brush hedefFirca;
+      private Random rnd = new Random();
+      
+      public OyunForm()
+      {
+         InitializeComponent();
+      }
+
+      private void Zamanlayici_Tick(object sender, EventArgs e)
+      {
+         panelGrafik.Clear(ayarlar.BosRenk);
+
+         int x = rnd.Next(OyunPanel.Width - hedefBoyutu);
+         int y = rnd.Next(OyunPanel.Height - hedefBoyutu);
+
+         panelGrafik.FillRectangle(hedefFirca, new Rectangle(x, y, hedefBoyutu, hedefBoyutu));
+      }
+
+      private void OyunForm_Load(object sender, EventArgs e)
+      {
+         ayarlar = new Properties.Settings();
+         ayarlar.Reload();
+         
+         Zamanlayici.Interval = ayarlar.ZamanAralik;
+         hedefFirca = new SolidBrush(ayarlar.HedefRengi);
+         OyunPanel.BackColor = ayarlar.BosRenk;
+
+         panelGrafik = OyunPanel.CreateGraphics();
+      }
+
+      private void btnBaslat_Click(object sender, EventArgs e)
+      {
+         Zamanlayici.Start();
+      }
+
+      private void tbZamanAralik_KeyDown(object sender, KeyEventArgs e)
+      {
+         if (e.KeyCode == Keys.Enter)
+         {
+            string str = tbZamanAralik.Text;
+            try
+            {
+               int sayi = Convert.ToInt32(str);
+
+               if (sayi >= ayarlar.MinimumAralik && sayi <= ayarlar.MaximumAralik)
+               {
+                  ayarlar.ZamanAralik = sayi;
+                  Zamanlayici.Interval = ayarlar.ZamanAralik;
+                  ayarlar.Save();
+               }
+            }
+            catch { }
+         }
+      }
+   }
+}
