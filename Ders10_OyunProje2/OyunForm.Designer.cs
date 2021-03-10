b695b27f3c2252934674e5d42a74cc86fd393f31
Hürol Aslan <freebelion@gmail.com>
2021-03-10 14:08:20 +0300

    Ders10 projesi eklendi

---
+++ Ders10_OyunProje2/OyunForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Ders10_OyunProje2
+{
+   partial class OyunForm
+   {
+      /// <summary>
+      ///Gerekli tasarımcı değişkeni.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      ///Kullanılan tüm kaynakları temizleyin.
+      /// </summary>
+      ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer üretilen kod
+
+      /// <summary>
+      /// Tasarımcı desteği için gerekli metot - bu metodun 
+      ///içeriğini kod düzenleyici ile değiştirmeyin.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.components = new System.ComponentModel.Container();
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OyunForm));
+         this.kontrolPaneli = new System.Windows.Forms.Panel();
+         this.oyunPaneli = new System.Windows.Forms.Panel();
+         this.resimListe = new System.Windows.Forms.ImageList(this.components);
+         this.numMatrisBoyutu = new System.Windows.Forms.NumericUpDown();
+         this.kontrolPaneli.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.numMatrisBoyutu)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // kontrolPaneli
+         // 
+         this.kontrolPaneli.BackColor = System.Drawing.Color.Gainsboro;
+         this.kontrolPaneli.Controls.Add(this.numMatrisBoyutu);
+         this.kontrolPaneli.Dock = System.Windows.Forms.DockStyle.Left;
+         this.kontrolPaneli.Location = new System.Drawing.Point(0, 0);
+         this.kontrolPaneli.Name = "kontrolPaneli";
+         this.kontrolPaneli.Size = new System.Drawing.Size(75, 450);
+         this.kontrolPaneli.TabIndex = 0;
+         // 
+         // oyunPaneli
+         // 
+         this.oyunPaneli.AllowDrop = true;
+         this.oyunPaneli.BackColor = System.Drawing.Color.White;
+         this.oyunPaneli.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.oyunPaneli.Location = new System.Drawing.Point(75, 0);
+         this.oyunPaneli.Name = "oyunPaneli";
+         this.oyunPaneli.Size = new System.Drawing.Size(464, 450);
+         this.oyunPaneli.TabIndex = 1;
+         // 
+         // resimListe
+         // 
+         this.resimListe.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("resimListe.ImageStream")));
+         this.resimListe.TransparentColor = System.Drawing.Color.Transparent;
+         this.resimListe.Images.SetKeyName(0, "renk1.bmp");
+         this.resimListe.Images.SetKeyName(1, "renk2.bmp");
+         this.resimListe.Images.SetKeyName(2, "renk3.bmp");
+         // 
+         // numMatrisBoyutu
+         // 
+         this.numMatrisBoyutu.Location = new System.Drawing.Point(13, 13);
+         this.numMatrisBoyutu.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+         this.numMatrisBoyutu.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+         this.numMatrisBoyutu.Name = "numMatrisBoyutu";
+         this.numMatrisBoyutu.Size = new System.Drawing.Size(53, 26);
+         this.numMatrisBoyutu.TabIndex = 0;
+         this.numMatrisBoyutu.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+         this.numMatrisBoyutu.ValueChanged += new System.EventHandler(this.numMatrisBoyutu_ValueChanged);
+         // 
+         // OyunForm
+         // 
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+         this.ClientSize = new System.Drawing.Size(539, 450);
+         this.Controls.Add(this.oyunPaneli);
+         this.Controls.Add(this.kontrolPaneli);
+         this.DoubleBuffered = true;
+         this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+         this.Name = "OyunForm";
+         this.Text = "Form1";
+         this.Load += new System.EventHandler(this.OyunForm_Load);
+         this.kontrolPaneli.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.numMatrisBoyutu)).EndInit();
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Panel kontrolPaneli;
+      private System.Windows.Forms.Panel oyunPaneli;
+      private System.Windows.Forms.ImageList resimListe;
+      private System.Windows.Forms.NumericUpDown numMatrisBoyutu;
+   }
+}
+
---
+++ Ders10_OyunProje2/OyunForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders10_OyunProje2
+{
+   public partial class OyunForm : Form
+   {
+      Properties.Settings ayarlar;
+      PictureBox[,] resimler;
+      Random rnd;
+      PictureBox kaynakKutu;
+      PictureBox hedefKutu;
+      public OyunForm()
+      {
+         InitializeComponent();
+      }
+
+      private void OyunForm_Load(object sender, EventArgs e)
+      {
+         rnd = new Random((int)DateTime.Now.Ticks);
+         ayarlar = new Properties.Settings();
+         numMatrisBoyutu.Value = ayarlar.MatrisBoyutu;
+
+         OyunPaneliDuzenle();
+      }
+
+      private void OyunPaneliDuzenle()
+      {
+         oyunPaneli.SuspendLayout();
+         ayarlar.Reload();
+         int N = ayarlar.MatrisBoyutu;
+         resimler = new PictureBox[N, N];
+         int boyut = Math.Min(oyunPaneli.Width, oyunPaneli.Height) / N;
+
+         PictureBox kutu;
+         for(int i = 0; i < N; i++)
+         {
+            for(int j = 0; j < N; j++)
+            {
+               resimler[i, j] = kutu = new PictureBox();
+               kutu.Left = j * boyut;
+               kutu.Top = i * boyut;
+               kutu.Width = boyut;
+               kutu.Height = boyut;
+               kutu.SizeMode = PictureBoxSizeMode.Zoom;
+               kutu.AllowDrop = true;
+               kutu.MouseDown += Kutu_MouseDown;
+               kutu.DragEnter += Kutu_DragEnter;
+               kutu.DragDrop += Kutu_DragDrop;
+               kutu.Image = resimListe.Images[rnd.Next(resimListe.Images.Count)];
+               oyunPaneli.Controls.Add(kutu);
+            }
+         }
+         oyunPaneli.ResumeLayout();
+      }
+
+      private void Kutu_DragDrop(object sender, DragEventArgs e)
+      {
+         hedefKutu = sender as PictureBox;
+         kaynakKutu = e.Data.GetData(typeof(PictureBox)) as PictureBox;
+
+         Image tmp = hedefKutu.Image;
+         hedefKutu.Image = kaynakKutu.Image;
+         kaynakKutu.Image = tmp;
+      }
+
+      private void Kutu_DragEnter(object sender, DragEventArgs e)
+      {
+         if(e.Data.GetDataPresent(typeof(PictureBox)))
+         { e.Effect = DragDropEffects.Move; }
+         else { e.Effect = DragDropEffects.None; }
+      }
+
+      private void Kutu_MouseDown(object sender, MouseEventArgs e)
+      {
+         kaynakKutu = sender as PictureBox;
+         kaynakKutu.DoDragDrop(kaynakKutu, DragDropEffects.Move);
+      }
+
+      private void numMatrisBoyutu_ValueChanged(object sender, EventArgs e)
+      {
+         ayarlar.MatrisBoyutu = (int) numMatrisBoyutu.Value;
+         ayarlar.Save();
+         oyunPaneli.Controls.Clear();
+         OyunPaneliDuzenle();
+      }
+   }
+}
