e6eb44ee0298a23c274958f61b1d8fb9bd71c791
Hürol Aslan <freebelion@gmail.com>
2021-03-24 14:14:19 +0300

    Ders14 projesi eklendi.

---
+++ Ders14_ListeKutusu/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ders14_ListeKutusu
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      private Random rnd = new Random();
+      
+      public ObservableCollection<Yuk> Yukler { get; set; }
+
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void yukAl(object sender, RoutedEventArgs e)
+      {
+         int yukSayisi = 1 + rnd.Next(10);
+
+         Yuk yeniYuk;
+         for(int i=0; i < yukSayisi; i++)
+         {
+            yeniYuk = new Yuk(10 * rnd.NextDouble());
+            Yukler.Add(yeniYuk);
+         }
+      }
+
+      private void yukGonder(object sender, RoutedEventArgs e)
+      {
+         int yukSayisi = 1 + rnd.Next(10);
+
+         yukSayisi = Math.Min(Yukler.Count, yukSayisi);
+
+         while(yukSayisi > 0)
+         {
+            Yukler.RemoveAt(0);
+            yukSayisi--;
+         }
+      }
+
+      private void Window_Loaded(object sender, RoutedEventArgs e)
+      {
+         Yukler = new ObservableCollection<Yuk>();
+         this.DataContext = this;
+      }
+   }
+}
---
+++ Ders14_ListeKutusu/Yuk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders14_ListeKutusu
+{
+   public class Yuk
+   {
+      private double yukAgirlik;
+
+      public Yuk(double ilkAgirlik)
+      {
+         yukAgirlik = ilkAgirlik;
+      }
+
+      public double Agirlik
+      {
+         get { return yukAgirlik; }
+      }
+
+      public override string ToString()
+      {
+         return yukAgirlik.ToString("0.000");
+      }
+   }
+}
