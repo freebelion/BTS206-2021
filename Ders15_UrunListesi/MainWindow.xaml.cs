864f981a8b77fb8beb6bea6bb91acd9a5c429068
Hürol Aslan <freebelion@gmail.com>
2021-03-29 16:31:59 +0300

    Ders15 projesi eklendi

---
+++ Ders15_UrunListesi/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Ders15_UrunListesi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Urun> urunler;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            urunler = new List<Urun>();
+
+            StreamReader okuyucu = new StreamReader("Urunler.txt");
+            string satir;
+            string[] bilgiler;
+            char[] ayiraclar = new char[] { ' ' };
+
+            Urun yeniUrun;
+            while (!okuyucu.EndOfStream)
+            {
+                satir = okuyucu.ReadLine();
+                bilgiler = satir.Split(ayiraclar);
+                yeniUrun = new Urun(bilgiler[0], Convert.ToDouble(bilgiler[1]));
+                urunler.Add(yeniUrun);
+            }
+
+            lbUrunler.ItemsSource = urunler;
+            lbUrunler.DisplayMemberPath = "Ad";
+        }
+
+        private void lbUrunler_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Urun secilenUrun = lbUrunler.SelectedItem as Urun;
+            // tbUrun.Text = secilenUrun.Fiyat.ToString("C2");
+        }
+    }
+}
---
+++ Ders15_UrunListesi/Urun.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders15_UrunListesi
+{
+    public class Urun
+    {
+        private string urunAdi;
+        private double urunFiyati;
+
+        public Urun(string uad, double ufiyat)
+        {
+            urunAdi = uad;
+            Fiyat = ufiyat;
+        }
+
+        public double Fiyat
+        {
+            get { return urunFiyati; }
+            set
+            {
+                if(value > 0)
+                { urunFiyati = value; }
+            }
+        }
+
+        public string Ad
+        {
+            get { return urunAdi; }
+        }
+    }
+}
