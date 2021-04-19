a1c3e57543f86f0280163bb182b2db3b1f6ff2b9
Hürol Aslan <freebelion@gmail.com>
2021-04-19 15:42:01 +0300

    Ders19 projesi eklendi

---
+++ Ders19_BankaProje2/Hesap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+
+namespace Ders19_BankaProje2
+{
+    public class Hesap : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OzellikDegisti(string ozellikAdi)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(ozellikAdi));
+            }
+        }
+
+        private decimal m_bakiye;
+
+        public decimal Bakiye
+        {
+            get { return m_bakiye; }
+        }
+
+        public void ParaEkle(decimal tutar)
+        {
+            m_bakiye += tutar;
+            OzellikDegisti("Bakiye");
+        }
+
+        public void ParaCek(decimal tutar)
+        {
+            if(m_bakiye >= tutar)
+            {
+                m_bakiye -= tutar;
+                OzellikDegisti("Bakiye");
+            }
+        }
+    }
+
+    public class HesapListe : ObservableCollection<Hesap> { }
+
+}
---
+++ Ders19_BankaProje2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ders19_BankaProje2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ParaEkle_Click(object sender, RoutedEventArgs e)
+        {
+            Hesap aktifHesap = (sender as Button).Tag as Hesap;
+            aktifHesap.ParaEkle(10);
+        }
+
+        private void ParaCek_Click(object sender, RoutedEventArgs e)
+        {
+            // AktifHesap.ParaCek(10);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for(int i=0; i < 5; i++)
+            {
+                Hesaplar.Add(new Hesap());
+            }
+        }
+    }
+}
